35dded5234ad69fc9a664f58282653624ab90de2
hraj37378 <hraj37378@gmail.com>
2024-04-14 00:01:24 +0530

    Conversions

---
+++ 3_conversionOperations.js
@@ -0,0 +1,17 @@
+// let score = "33"
+// let score = "33abc"
+// console.log(typeof score);
+let score = 33
+// let valueInNumber = Number(score)
+// let boolValue = Boolean(score)
+let stringVal = String(score)
+
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  
+// let score = "33abc" is NaN type that is Not a Number
+
+// number and string to boolean
+// 1 => true ; 0 => false
+// "" => false
+// "fgdf" => true
