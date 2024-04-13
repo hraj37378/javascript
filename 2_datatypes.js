801dc0d6be9b7ca0c63aa3174fa67c75d74afddd
hraj37378 <hraj37378@gmail.com>
2024-04-13 23:48:07 +0530

    Data  types

---
+++ 2_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" ;// treat all JS code as newer version 
+
+// alert(3 + 3) // we are using nodejs not browser 
+let name = "Harsh"
+let age  = 22
+let isLoggedin = false
+
+// number type 
+// bigint for really large number 
+// string => ""
+// boolean => true/false
+// null => standalone value , its type is OBJECT 
+// undefined => let state; {state is undefined as of now }
+// symbol => unique , used mainly in react
+// object 
+console.log(typeof null)
