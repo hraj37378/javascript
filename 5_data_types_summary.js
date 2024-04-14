13fab3772bb311421ba222d9eb68c01dd7821ebc
hraj37378 <hraj37378@gmail.com>
2024-04-14 13:21:49 +0530

    Updated file

--- 5_data_types_summary.js
+++ 5_data_types_summary.js
@@ -7,3 +7,26 @@
 // Array , Objects , Functions  
 // **********************************
 //JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+// +++++++++++++++++++++++++++++++++
+// Stack (Primitive)  Heap (Non Primitive )
+
+let name1 = "Harsh Raj"
+let name2 = name1
+name2 = "Kishan Kumar"
+
+console.log(name1);
+console.log(name2);
+
+// the change will be only in name2 variable . The concept here is that it is primitive data type and it is stored in stack ehich copies the value of name1 in name2 and not the refrence .
+
+let userOne = {
+    email:"hraj@gmail.com",
+    gender : "male"
+};
+
+let userTwo = userOne;
+userTwo.email = "kishan@gmail.com";
+console.log(userOne.email);
+console.log(userTwo.email);
+// Objects are Non Primitive and stored in heap memory which stores the reference . So any change in any value will reflect in the original one too. 
