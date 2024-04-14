88bd451f679a32c2635e777c34ada63e767dc5fd
hraj37378 <hraj37378@gmail.com>
2024-04-14 12:16:53 +0530

    Data types summary

---
+++ 5_data_types_summary.js
@@ -0,0 +1,7 @@
+// there are two data types based on how data is stored in memory and how is is extracted 
+
+// PRIMITIVE : 
+// 7 types : String , Number Boolean , null , undefined , Symbol , BigInt 
+
+// REFERENCE or NON PRIMITIVE : memory me reference allocate kiya jata hai
+// Array , Objects , Functions  
